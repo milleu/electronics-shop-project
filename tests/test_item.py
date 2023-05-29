9949fb14c67afe22041c50b7df9233dd50716976
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-05-29 16:52:03 -0700

    tests are done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,9 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+def test_calculate_total_price():
+    assert Item("Смартфон", 10000, 20).calculate_total_price() == 200000
+
+def test_apply_discount():
+    assert Item("Смартфон", 10000, 20).apply_discount(0.8) == 8000.0
