7fe42d282cba6f8ced67ab5a19db7d66c4ea5ac5
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-05-30 23:09:29 -0700

    tests are done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,9 +1,13 @@
 import pytest
 
 from src.item import Item
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
 
 def test_calculate_total_price():
     assert Item("Смартфон", 10000, 20).calculate_total_price() == 200000
 
-def test_apply_discount():
-    assert Item("Смартфон", 10000, 20).apply_discount(0.8) == 8000.0
+def test_apply_discount(item):
+    item.apply_discount()
+    assert item.price == 10000.0
