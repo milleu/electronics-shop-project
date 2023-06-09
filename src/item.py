f0a7768214605abaa5b9cc249470fc054d059e85
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-06-09 13:46:08 -0700

    all functions were written

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -12,7 +15,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -23,7 +26,7 @@ class Item:
         """
         return self.price * self.quantity
 
-    def apply_discount(self, ) -> float:
+    def apply_discount(self ) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
@@ -32,7 +35,39 @@ class Item:
 
 
     def add_all(self):
-        self.all.append(self.name)
+        self.all.append(self.__name)
         self.all.append(self.price)
         self.all.append(self.quantity)
         return all
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, item_name: str):
+        if len(item_name) > 10:
+            raise ValueError("Длина наименования товара превышает 10 символов.")
+        else:
+            self.__name = item_name
+    @classmethod
+    def instantiate_from_csv(csl):
+        with open("../src/items.csv", newline="") as file:
+            reader = csv.reader(file)
+            for string in reader:
+                if string[0] != "name":
+                    __name, price, quantity = string[0], string[1], string[2]
+                    item1 = Item(__name, price, quantity)
+                    csl.all.append(item1)
+
+    def __repr__(self) ->str:
+        return f"Item(name='{self.name}', price='{self.price}, quantity='{self.quantity}')"
+    @staticmethod
+    def string_to_number(string: str):
+        """статический метод, возвращающий число из числа-строки"""
+        num = float(string)
+        int_num = int(num)
+        return int_num
+
+
+
+
+
